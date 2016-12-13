e8b59ab747cda59a40c165b9426e3ae35e24c39a
lwd <1132438569@qq.com>
2016-12-13 17:31:00 +0800

    empi模块提供了基本的连接数据库功能

--- dxc/code/empi_test.py
+++ dxc/code/empi_test.py
@@ -1,17 +1,30 @@
 #-*- coding: utf-8 -*-
 
+#找重复值的SQL
+#SELECT z.patient_name name , COUNT(z.patient_name) FROM `zmap_r_patient` as z GROUP BY z.patient_name HAVING COUNT(z.patient_name) > 1 ;
+
 import pandas as pd
 import matplotlib.pyplot as plt
+import pymysql
 
-inputfile = '../data/patient.xlsx'
+# inputfile = '../data/patient.xlsx'
+#
+# data = pd.read_excel(inputfile)  # 导入数据
+#
+# for index,row in data.iterrows():
+#     for col_name in data.columns:
+#         if(row[col_name] == u'海凤英'):
+#             print row
 
-data = pd.read_excel(inputfile)  # 导入数据
+conn = pymysql.connect(host = '139.196.198.56', user='root' , passwd='Jth2016', db = 'zmap_empi', charset = 'utf8')
+cur = conn.cursor()
 
-for index,row in data.iterrows():
-    for col_name in data.columns:
-        if(row[col_name] == u'海凤英'):
-            print row
-
+# cur.execute("select * from zmap_r_patient limit 10")
+query = "SELECT z.patient_name name , COUNT(z.patient_name) FROM zmap_r_patient as z GROUP BY z.patient_name HAVING COUNT(z.patient_name) > 1"
+cur.execute(query)
+result = cur.fetchall()
+for i in range(result.__len__()):
+        print result[i][0],result[i][1]
 # for col_name in data.columns:
 #     print col_name
 # def map(data, exp):
